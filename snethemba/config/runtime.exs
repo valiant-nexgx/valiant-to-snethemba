2dceeac9c73021331f8c71bbdccceca7d9800611
Valiant K. <khoza.valiantr@advannotech.co.za>
2026-02-14 12:24:20 +0200

    Add Docker configuration and release management scripts for Snethemba application

--- snethemba/config/runtime.exs
+++ snethemba/config/runtime.exs
@@ -34,11 +34,10 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :snethemba, Snethemba.Repo,
-    # ssl: true,
+    ssl: true,
+    ssl_opts: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    # For machines with several cores, consider starting multiple pools of `pool_size`
-    # pool_count: 4,
     socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
